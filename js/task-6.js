dacd7f17f3a1d8b640cfeddd1cb3f9bad703abbc
FreJa90 <122282171+FreJa90@users.noreply.github.com>
2024-05-07 09:50:55 +0100

    color_and_size

--- js/task-6.js
+++ js/task-6.js
@@ -8,16 +8,18 @@ const container = document.querySelector("#boxes");
 const inputField = document.querySelector("input");
 const btnCreate = document.querySelector('button[data-create]');
 const btnDestroy = document.querySelector('button[data-destroy]');
+let boxBeggin =  30;
 let amount = [];
 console.log(amount);
 
 
 function createBoxes(amount) {
   const box = document.createElement("div");
-  box.style.backgroundColor =  "red";
-  box.style.width = "30px";
-  box.style.height = "30px";
+  box.style.backgroundColor = getRandomHexColor();
+  box.style.width = `${ boxBeggin }px`;
+  box.style.height = `${boxBeggin}px`;
   container.style.display = "flex";
+  container.style.flexWrap = "wrap";
   container.style.gap = "44px"
   container.style.marginTop = "16px";
   container.prepend(box);
@@ -26,14 +28,15 @@ function createBoxes(amount) {
 inputField.addEventListener("input", handleInput);
 function handleInput(event) {
   amount.push(event.target.value);
-  // inputField.reset();
+  // input.reset();
 };
 
 btnCreate.addEventListener("click", handleCreate);
 
 function handleCreate(event) {
   amount.forEach(element => {
-    createBoxes(element);
+    createBoxes(element)
+    boxBeggin += 10;
   })
   
 };
